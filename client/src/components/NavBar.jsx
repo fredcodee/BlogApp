606f6cf8eb06bab7119c025596273e1eef3bf3de
fredcodee <fredcode23@gmail.com>
2023-05-12 12:17:27 +0800

    redesign(nav bars)

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import logo from '../assets/images/logo.jpg'
 import { Link } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
@@ -6,32 +6,37 @@ import AuthContext from '../context/AuthContext'
 const NavBar = () => {
   let { user } = useContext(AuthContext)
   return (
-    <div className='bg-brightBlue font-mono'>
+    <div className=''>
       <nav className="relative container mx-auto p-6">
         <div className="flex items-center justify-between">
-            <div className="flex items-center pt-2">
-                <img src={logo} className="w-12 h-12" />
-                <h2 className='text-white text-lg font-bold'>Fredcode</h2>
+          <div className="flex items-center pt-2">
+            <img src={logo} className="w-12 h-12 rounded-full" />
+            <h2 className='text-2xl font-bold'>Fredcode</h2>
+          </div>
+
+          {user ? (
+            <div className="hidden md:flex space-x-10">
+              <Link to="/blog" className="text-slate-600 font-medium hover:text-darkOrange text-lg no-underline">Blog</Link>
+              <Link to="/admin-dashboard" className="text-slate-600 font-medium hover:text-darkOrange text-lg no-underline">Admin-Dashboard</Link>
+              <Link to="/blog" className="text-slate-600 font-medium hover:text-darkOrange text-lg no-underline">New Post</Link>
+              <Link to="/contact" className="text-slate-600 font-medium hover:text-darkOrange text-lg no-underline">User Management</Link>
+              <Link to="/logout" className=" font-medium text-red-500 hover:text-blue text-lg no-underline">Logout</Link>
             </div>
-            
-              {user ? (
-                <div className="hidden md:flex space-x-10">
-                  <Link to="/blog" className="text-white hover:text-darkOrange text-lg no-underline">Blog</Link>
-                  <Link to="/admin-dashboard" className="text-white hover:text-darkOrange text-lg no-underline">Admin-Dashboard</Link>
-                  <Link to="/blog" className="text-white hover:text-darkOrange text-lg no-underline">New Post</Link>
-                  <Link to="/contact" className="text-white hover:text-darkOrange text-lg no-underline">User Management</Link>
-                  <Link to="/logout" className="text-red-500 hover:text-darkOrange text-lg no-underline">Logout</Link>
-                </div>
-              ):(
-                <div className="hidden md:flex space-x-10">
-                  <Link to="/" className="text-white hover:text-darkOrange text-lg no-underline">Home</Link>
-                  <Link to="/blog" className="text-white hover:text-darkOrange text-lg no-underline">Blog</Link>
-                  <Link to="/contact" className="text-white hover:text-darkOrange text-lg no-underline">Contact</Link>
-                </div>
-              )}  
-            
+          ) : (
+            <div className="hidden md:flex space-x-10">
+              <Link to="/" className="text-slate-600 font-mediu hover:text-darkOrange text-lg no-underline">Home</Link>
+              <Link to="/blog" className="text-slate-600 font-medium hover:text-darkOrange text-lg no-underline">Blog</Link>
+              <Link to="/contact" className="text-slate-600 font-mediu hover:text-darkOrange text-lg no-underline">Contact</Link>
+              <a href="/" className='flex flex-wrap items-center gap-2 group tracking-tight font-medium cursor-pointer  rounded-lg px-3 py-2 text-lg bg-orange-300 text-secondary-900 hover:bg-blue-400 undefined w-auto'>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class=" transition-transform h-6 w-6  order-last group-hover:translate-x-2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M336 176h40a40 40 0 0140 40v208a40 40 0 01-40 40H136a40 40 0 01-40-40V216a40 40 0 0140-40h40"></path><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M176 272l80 80 80-80M256 48v288"></path></svg>
+                <span className='text-white'>My Cv</span>
+              </a>
+            </div>
+          )}
+
         </div>
       </nav>
+      <hr />
     </div>
   )
 }
