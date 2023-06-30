90f14414f8930c9f1f938cbea9a01aa765f7ff96
fredcode <fredcode23@gmail.com>
2023-06-30 23:35:10 +0800

    fixed login page error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Home = () => {
+  //go to different website
+  window.location.href = "https://fredcodee.github.io/portfolio/"
+
   return (
     <div>
       <h1>Hompe page(portfolio)</h1>
