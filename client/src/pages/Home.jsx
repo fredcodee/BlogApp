05773f99f972a00024149e8ff8b66edf0fde8a44
fredcode <fredcode23@gmail.com>
2023-06-30 23:43:21 +0800

    fixind deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   //go to different website
-  window.location.href = "https://fredcodee.github.io/portfolio/"
+  // window.location.href = "https://fredcodee.github.io/portfolio/"
 
   return (
     <div>
