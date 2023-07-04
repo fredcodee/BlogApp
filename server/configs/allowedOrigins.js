69426eca47cb0d76a5f62a8d67dbaa3aabae4129
fredcode <fredcode23@gmail.com>
2023-07-04 11:21:36 +0800

    cors for backend

--- server/configs/allowedOrigins.js
+++ server/configs/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     process.env.FRONTEND_ORIGIN_DEV,
+    process.env.FRONTEND_ORIGIN_DEV2,
     process.env.FRONTEND_ORIGIN_PROD,
     process.env.FRONTEND_ORIGIN_PROD2,
 ]
