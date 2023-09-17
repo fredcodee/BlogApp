7f162f71dd28608013e6c734a272c57b6dca6ca8
fredcode <fredcode23@gmail.com>
2023-09-18 03:07:02 +0800

    init portfolio

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import logo from '../assets/images/logo.jpg'
 import { Link} from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
-import Resume from '../assets/files/resume.pdf'
+
 
 const NavBar = () => {
   let { user } = useContext(AuthContext)
@@ -34,18 +34,16 @@ const NavBar = () => {
           ) : (
             <div className="hidden md:flex space-x-10">
               <Link to="/" className="text-slate-600 font-mediu hover:text-darkOrange text-lg no-underline">Home</Link>
+              <Link to="#experience" className="text-slate-600 font-medium hover:text-darkOrange text-lg no-underline">Experience</Link>
+              <Link to="#" className="text-slate-600 font-medium hover:text-darkOrange text-lg no-underline">Projects</Link>
               <Link to="/blog" className="text-slate-600 font-medium hover:text-darkOrange text-lg no-underline">Blog</Link>
               <Link to="/contact" className="text-slate-600 font-mediu hover:text-darkOrange text-lg no-underline">Contact</Link>
-              <a href={Resume} download className='flex flex-wrap items-center gap-2 group tracking-tight font-medium cursor-pointer  rounded-lg px-3 py-2 text-lg bg-orange-300 text-secondary-900 hover:bg-blue-400 undefined w-auto'>
-              <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" className=" transition-transform h-6 w-6  order-last group-hover:translate-x-2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M336 176h40a40 40 0 0140 40v208a40 40 0 01-40 40H136a40 40 0 01-40-40V216a40 40 0 0140-40h40"></path><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M176 272l80 80 80-80M256 48v288"></path></svg>
-                <span className='text-white'>My Cv</span>
-              </a>
+              
             </div>
           )}
 
         </div>
       </nav>
-      <hr />
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,141 @@
 import React from 'react'
+import Resume from '../assets/files/resume.pdf'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCircleCheck} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import aboutMeImg from '../assets/images/aboutme.jpg'
 
 const Home = () => {
   //go to different website
   //window.location.href = "https://www.google.com"
-  
+
   return (
-    <div>
-      <h1>Hompe page(portfolio)</h1>
+    <div className='container'>
+      <div className='text-center p-5'>
+        <section id="intro" className='pb-5 pt-5'>
+          <div className="inner">
+            <div>
+              <p className='font-bold text-slate-500'>Hello, I'm</p>
+              <p className='text-5xl font-bold'>Wilfred Chukwu</p>
+              <p className='text-3xl text-slate-500'>Software Engineer </p>
+              <div>
+                <a href={Resume} download className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-400 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Download Resume</a>
+                <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-400 dark:focus:ring-gray-700 dark:border-gray-700">Contact Me</button>
+              </div>
+
+              <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
+                <a href="https://www.linkedin.com/in/wilfred-chukwu-891830174/" className='text-3xl'><FontAwesomeIcon icon={faLinkedin} style={{ color: "#1e1e1f" }} /></a>
+                <a href="https://github.com/fredcodee" className='text-3xl'><FontAwesomeIcon icon={faGithub} style={{ color: "#212121" }} /></a>
+              </div>
+            </div>
+            <a href="#main" className="more text-stone-50 hover:text-white ">View My Projects</a>
+
+          </div>
+        </section>
+      </div>
+
+
+      <div className='pt-5'>
+        <section id='About' className='pb-5'>
+          <div className='text-center pt-4 pb-5'>
+            <p>Get to know more</p>
+            <h1 className='font-bold'>About Me</h1>
+          </div>
+          <div className="grid grid-cols-3 gap-4 pt-5">
+            <div className="...">
+              <img src={aboutMeImg} alt="image" className='rounded-lg' />
+            </div>
+            <div className="col-span-2 ..." style={{justifyContent: 'center', alignItems:'center', alignContent:'center'}}>
+              <div className="grid grid-cols-2 gap-4 text-center pt-5">
+                <div className="...">
+                  <div className='border-solid'>
+                    <p>I'm a Software Engineer and I have developed scalable, secure and object-oriented web applications, with a focus on efficient algorithm usage and optimal database structures. I'm confident, naturally curious, and perpetually working on improving my skills, mindset and growth.</p>
+                  </div>
+                </div>
+                <div className="...">
+                  <div className='border-solid'>
+                    <p className='font-bold'>Experience</p>
+                    <p>4+ years <br /><span>Software Engineer</span></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </section>
+      </div>
+
+
+      <div>
+        <section id='experience' className='pb-5'>
+          <div className='text-center pt-4 pb-5'>
+            <p>Explore My</p>
+            <h1 className='font-bold'>Experience</h1>
+          </div>
+          <div className="grid grid-cols-2 gap-4 text-center pt-5">
+            <div className="...">
+              <div className='border-solid border-2 border-gray-500 rounded-lg p-4'>
+                <h3 className='text-slate-500'>Frontend Development</h3>
+                <div className="grid grid-cols-3 gap-4 text-center pt-5">
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>HTML</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>Css</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>Sass</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>Tailwind & Boostrap</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>Javascript</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>React</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="...">
+              <div className='border-solid border-2 border-gray-500 rounded-lg p-4'>
+                <h3 className='text-slate-500'>Backend Development</h3>
+                <div className="grid grid-cols-3 gap-4 text-center pt-5">
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>MongoDb</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>PostgreSQL</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>Node Js</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>Express Js</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>Django</p>
+                  </div>
+                  <div className="...">
+                    <p><span><FontAwesomeIcon icon={faCircleCheck} style={{color: "#0f0f0f",}}  className='pr-2'/></span>Git</p>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div className='pt-5'>
+        <section id='projects'>
+        <div className='text-center pt-4 pb-5'>
+            <p>Browse My Recent</p>
+            <h1 className='font-bold'>Projects</h1>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
